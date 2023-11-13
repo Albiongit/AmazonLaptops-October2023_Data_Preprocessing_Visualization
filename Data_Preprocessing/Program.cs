3cdde287318cc55b95d0936fb686dc6db0a40456
Albion Ademi <albion.ademi@comitas.com>
2023-11-14 00:28:22 +0100

    Add project files.

---
+++ Data_Preprocessing/Program.cs
@@ -0,0 +1,49 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+public class Program
+{
+    static void Main()
+    {
+        // Specify the path to dataset CSV file
+        string csvFile = GetCsvFilePath("SharedData", "Data", "amazon_laptops.csv");
+
+        // Read the CSV file and create a list of dynamic objects - take only first 10 records for testing
+        List<dynamic> data = ReadCsv(csvFile);
+
+        // Display data in records
+        DisplayData(data);
+    }
+
+    static List<dynamic> ReadCsv(string filePath)
+    {
+        using (var reader = new StreamReader(filePath))
+        using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+        {
+            return csv.GetRecords<dynamic>().Take(10).ToList();
+        }
+    }
+
+    static string GetCsvFilePath(params string[] paths)
+    {
+        string solutionDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
+
+        return Path.GetFullPath(Path.Combine(solutionDirectory, Path.Combine(paths)));
+    }
+
+    static void DisplayData(List<dynamic> data)
+    {
+        foreach (var item in data)
+        {
+            Console.WriteLine("--------------- Record ---------------");
+
+            foreach (var property in item)
+            {
+                Console.WriteLine($"{property.Key}: {property.Value}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
